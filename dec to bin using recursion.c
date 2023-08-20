c23cad3df19e72887411646967b45d6d6c233e32
malikburhan-21 <107347962+malikburhan-21@users.noreply.github.com>
2023-08-20 16:41:35 +0530

    Add files via upload

---
+++ dec to bin using recursion.c
@@ -0,0 +1,27 @@
+//   W.A.P in c to convert decimal number to binary  using " recursion".  input should be taken from the user 
+
+#include<stdio.h>
+void binary(int);
+int main()
+{
+	int n;
+	printf("enter decimal number for conversion to binary\n");
+	scanf("%d",&n);
+	binary(n);
+	return 0;
+}
+
+void binary(int n)
+{
+	if(n==0)
+	printf("0");
+	
+	else if(n==1)
+	printf("1");
+	
+	else
+	{
+		binary(n/2);
+		printf("%d",n%2);
+	}
+}
